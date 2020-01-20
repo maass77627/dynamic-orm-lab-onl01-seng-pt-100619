b728cedf8c4246bef0f00ccac753b842b8fb9477
kelly r maass <kmaass77627@gmail.com>
2020-01-20 03:31:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,6 +18,11 @@ class InteractiveRecord
     column_names.compact
   end 
   
-  def self.initialize 
+  def initialize(options = {})
+    options.each do |property, value|
+      self.send ("#{property}=", value)
+  end 
+  end 
+
   
 end
