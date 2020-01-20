853147ef03196beca91a5dd764d1eca3e037b2e4
kelly r maass <kmaass77627@gmail.com>
2020-01-20 04:47:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,6 +32,10 @@ class InteractiveRecord
   self.class.column_names.delete_if {|col| col == "id"}.join(", ")
 end
 
+def values_for_insert
+  values = []
+  self.class.column_names.each do |col_name|
+    values << 
 
   
 end
